7bc3111237dae8a92c09ff84f34b84acd5c0a697
fvera87 <fernando.vl87@gmail.com>
2018-12-07 22:49:58 +0100

    fetch song by id, lyric creation

---
+++ client/components/LyricCreate.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react'
+import addLyricToSong from '../queries/addLyricToSong'
+import { graphql } from 'react-apollo'
+class LyricCreate extends Component {
+  constructor (props) {
+    super(props)
+    this.state = { content: '' }
+  }
+  onSubmit (event) {
+    event.preventDefault()
+    this.props.mutate({
+      variables: {
+        content: this.state.content,
+        songId: this.props.songId
+      }
+    }).then(() => this.setState({ content: '' }))
+  }
+  render () {
+    return (
+      <form onSubmit={this.onSubmit.bind(this)}>
+        <label>Add a lyric</label>
+        <input
+          value={this.state.content}
+          onChange={event => this.setState({ content: event.target.value })}
+        />
+      </form>
+    )
+  }
+}
+export default graphql(addLyricToSong)(LyricCreate)
---
+++ client/queries/addLyricToSong.js
@@ -0,0 +1,9 @@
+import gql from 'graphql-tag'
+export default gql`mutation AddLyricToSong($songId:ID!, $content:String) {
+  addLyricToSong(content: $content, songId: $songId) {
+    id,
+    lyrics {
+      content
+    }
+  }
+}`
